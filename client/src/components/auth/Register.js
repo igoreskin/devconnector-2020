bc12f690f62686c44906a73644e72962b4450f3b
Igor Eskin <eskinhd@gmail.com>
2021-01-19 15:22:49 -0500

    Setting up Redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
 
   const [formData, setFormData] = useState({
     name: '',
@@ -13,12 +16,12 @@ const Register = () => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2) {
-      console.log('Passwords do not match')
+      setAlert('Passwords do not match', 'danger')
     } else {
-      console.log(formData)
+      console.log('SUCCESS')
     }
   }
 
@@ -34,8 +37,7 @@ const Register = () => {
           <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
           <small className="form-text"
           >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
-          >
+            Gravatar email</small>
         </div>
         <div className="form-group">
           <input
@@ -60,10 +62,10 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   )
 }
 
-export default Register;
+export default connect(null, { setAlert })(Register);
---
+++ client/src/sagas/alert.js
@@ -0,0 +1,11 @@
+import { takeEvery, takeLatest, take, call, fork, put } from 'redux-saga/effects';
+import * as types from '../actions/types';
+import * as actions from '../actions/alert';
+
+function* setAlert() {
+  
+}
+
+const alertSagas = [
+   
+]
